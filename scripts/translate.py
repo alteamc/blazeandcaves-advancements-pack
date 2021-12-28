9724bf23282e7660bca9dd886ef402d0b7ee3202
dreamscached <dreamscache.d@gmail.com>
2021-12-28 15:59:52 +0100

    Improve translation script and add missing translations

--- scripts/translate.py
+++ scripts/translate.py
@@ -4,17 +4,21 @@ p = argparse.ArgumentParser(prog="translate", description="Apply translations fr
 p.add_argument("language", type=str, help="language to copy from translation pack into the datapack")
 args = p.parse_args()
 
-def translate(data, trans):
+def translate(data, trans, stat):
     if isinstance(data, dict):
         for key, value in data.items():
             if key == "translate":
+                stat[0] += 1
                 if value in trans:
                     data[key] = trans[value]
+                    stat[1] += 1
+                elif value in trans.values():
+                    stat[1] += 1
             else:
-                translate(value, trans)
+                translate(value, trans, stat)
     elif isinstance(data, list):
         for item in data:
-            translate(item, trans)
+            translate(item, trans, stat)
 
 DATA_DIR = "datapack/data/blazeandcave/advancements"
 TRANS_DIR = "translation/assets/minecraft/lang"
@@ -29,15 +33,21 @@ except FileNotFoundError:
 
 for _dir, _, files in os.walk(DATA_DIR):
     for _file in files:
-        print("Translating {0}... ".format(_file), end="")
-
         _file = os.path.join(_dir, _file)
+        print("Translating {0}... ".format(_file), end="")
         with open(_file, "r") as _in:
             data = hjson.load(_in)
 
-        translate(data, trans)
+        stat = [0, 0]
+        translate(data, trans, stat)
 
         with open(_file, "w") as out:
-            json.dump(data, out, ensure_ascii=False)
+            json.dump(data, out, ensure_ascii=False, indent=2)
 
-        print("OK.")
+        if stat[0] > 0:
+            if stat[0] > stat[1]:
+                print("INCOMPLETE.")
+            else:
+                print("OK.")
+        else:
+            print("UNTRANSLATABLE.")
